ed38f06a488e17ae42ecc250f8943ce0395ae6d2
ththfql <145094239+ththfql@users.noreply.github.com>
2026-04-15 17:27:04 +0900

    gg

--- week6/split_str.cpp
+++ week6/split_str.cpp
@@ -1,8 +1,7 @@
 ﻿#include<iostream>
 #include<string>
 #include<vector>
-#include<cctype>	//?먮즺?뺤쓽 ?⑥닔 ?щ윭 媛??덈떎
-//isspace(a)?⑥닔??a媛 怨듬갚?대㈃ 李?媛믪쓣 諛섑솚
+#include<cctype>	
 
 using namespace std;
 
@@ -21,28 +20,25 @@ vector<string> split(const string& s) {
 	typedef string::size_type str_sz;
 	str_sz i = 0;
 
-	//遺덈??? 吏湲덇퉴吏[?먮옒??i, ?꾩옱??i)踰붿쐞???덈뒗 臾몄옄?ㅼ쓣 泥섎━
+	
 	while (i != s.size()) {
-		//?좏뻾?섎뒗 怨듬갚?ㅼ쓣 臾댁떆, 遺덈??? 吏湲덇퉴吏[?먮옒??i, ?꾩옱??i)
-		// 踰붿쐞???덈뒗 臾몄옄?ㅼ쓣 紐⑤몢 怨듬갚(isspace() )
+		//(isspace() )
 		while (i != s.size() && isspace(s[i])) {
 			++i;
 		}
 
-		//?쒖꽌???ㅼ쓬 ?⑥뼱???앹쓣 李얠쓬
+		
 		str_sz j = i;
 
-		//?좏뻾?섎뒗 怨듬갚?ㅼ쓣 臾댁떆, 遺덈??? 吏湲덇퉴吏[?먮옒??i, ?꾩옱??i)
-		// 踰붿쐞???덈뒗 臾몄옄?ㅼ쓣 怨듬갚???꾨떂(isspace() )
+		//(isspace() )
 		while (j != s.size() && !isspace(s[j])) {
 			j++;
 		}
 
-		//怨듬갚???꾨땶 臾몄옄?ㅼ쓣 李俊英얠븯?꾨븣
 		if (i != j) {
-			//i?먯꽌遺??j-1媛쒖쓽 臾몄옄?ㅼ쓣 s??蹂듭궗
+			
 			ret.push_back(s.substr(i, j - 1));
-			i = j;	//j?몃뜳?ㅺ? i?몃뜳?ㅻ줈 ?대룞
+			i = j;	
 		}
 	}
 	return ret;
@@ -53,7 +49,7 @@ vector<string>ret;
 typedef string::const_iterator iter;	//수정
 iter i = s.begin();	//첫요소에 가리키는 포인터 (개념)
 
-//遺덈??? 吏湲덇퉴吏[?먮옒??i, ?꾩옱??i)踰붿쐞???덈뒗 臾몄옄?ㅼ쓣 泥섎━
+
 while (i != s.end()) {
 	//선행 공백을 무시
 	i = find_if(i, s.end(), not_space);
@@ -61,7 +57,7 @@ while (i != s.end()) {
 	//다음 단어의 끝을 찾음
 	iter j = find_if(i, s.end(), space);
 
-	//?좏뻾?섎뒗 怨듬갚?ㅼ쓣 臾댁떆, 遺덈??? 吏湲덇퉴吏[?먮옒??i, ?꾩옱??i)
+	
 	//문자들을 복사
 	if (i != s.end()) {
 		//i?먯꽌遺??j-1媛쒖쓽 臾몄옄?ㅼ쓣 s??蹂듭궗
@@ -92,3 +88,4 @@ int main(void) {
 	}
 	return 0;
 }
+
