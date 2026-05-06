62fda77f87d8d441c157e9eae43224486e7ae8c2
ththfql <145094239+ththfql@users.noreply.github.com>
2026-05-06 14:15:58 +0900

    .

--- week6/median.cpp
+++ week6/median.cpp
@@ -1,18 +1,30 @@
-﻿#include<vector>
-#include<algorithm>
-#include<stdexcept>
-#include"median.h"
+﻿// median.cpp
+#include <algorithm>
+#include <stdexcept>
+#include <vector>
 
-using namespace std;	//domain_error()?뚮Ц??
-double median(vector<double>vec) {
-	typedef vector<double>::size_type vec_sz;
+#include "grade.h"
+#include "Student_info.h"
 
-	vec_sz size = vec.size();
-	if (size == 0) {
-		throw domain_error("Median of empty vector!");
-		//return 1;
-	}
+using std::vector;
+using std::domain_error;
+using std::sort;
 
-	sort(vec.begin(), vec.end());
-	vec_sz mid = size / 2;
-	return size % 2 == 0	//鈺곌퀗援?		? (vec[mid] + vec[mid - 1]) / 2	//筌〓챷???獄쏆꼹??		: vec[mid];	//椰꾧퀣彛????獄쏆꼹??}
+// vector<double>의 중앙값을 구함.
+// 함수를 호출하면 인수로 제공된 백터를 통째로 복사
+double median(vector<double> vec) {
+    // 과제 점수의 입력 유무를 확인
+    typedef vector<double>::size_type vec_sz;
+    vec_sz size = vec.size();
+    if (size == 0) {
+        throw domain_error("Median of empty vector");
+    }
+
+    // 점수를 정렬
+    sort(vec.begin(), vec.end());
+
+    // 과제 점수의 중앙값을 구함
+    vec_sz mid = size / 2;
+    return size % 2 == 0
+        ? (vec[mid] + vec[mid - 1]) / 2 : vec[mid];
+}
